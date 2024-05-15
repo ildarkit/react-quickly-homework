9962897ca14944c17e93ba836b8179b441d75578
Ildar Shamiev <shamief@yandex.ru>
2024-05-15 14:13:32 +0300

    ch11 hw1

--- ch11/rq11-scaffold/src/Menu.js
+++ ch11/rq11-scaffold/src/Menu.js
@@ -1,5 +1,16 @@
+import MenuItem from "./MenuItem";
+
 function Menu() {
-  return <nav></nav>;
+  return (
+    <nav>
+      <ul className="menu">
+        <MenuItem href="/" icon="home">Home</MenuItem>
+        <MenuItem href="/services" icon="services">Services</MenuItem>
+        <MenuItem href="/pricing" icon="pricing">Pricing</MenuItem>
+        <MenuItem href="/blog" icon="blog">Blog</MenuItem>
+      </ul>
+    </nav>
+  );
 }
 
 export default Menu;
