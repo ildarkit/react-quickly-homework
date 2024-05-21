93cb7b7b53bcbea35127d11f67211fb1346a625d
Ildar Shamiev <shamief@yandex.ru>
2024-05-21 12:17:49 +0300

    ch12 hw4

--- ch12/timers-app/src/TimerManager.js
+++ ch12/timers-app/src/TimerManager.js
@@ -1,9 +1,28 @@
+import {useState} from "react";
 import Timer from "./Timer";
+import NewTimer from "./NewTimer";
 
 function TimerManager() {
+  const [timerId, setTimerId] = useState(1);
+  const [timers, setTimer] = useState([]);
+  const handleDelete = timerId => {
+    setTimer(timers.filter(timer => timer.timerId !== timerId));
+  };
+  const handleAdd = sec => {
+    if (sec === 0) return;
+    const timer = {
+      timerId: timerId,
+      startTime: sec,
+    };
+    setTimer(timers.concat(timer));
+    setTimerId(timerId + 1);
+  };
   return (
     <div className="timers">
-      <Timer startTime={300}/>
+      {timers.map(
+        timer => <Timer key={timer.timerId} handleDelete={handleDelete} {...timer}/>
+      )} 
+      <NewTimer handleAdd={handleAdd}/>
     </div>
   );
 }
