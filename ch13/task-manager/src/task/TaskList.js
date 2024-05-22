711b82d3b2173c6ada46c5306c674664fffad9f1
Ildar Shamiev <shamief@yandex.ru>
2024-05-22 17:22:33 +0300

    ch13 TaskList

--- ch13/task-manager/src/task/TaskList.js
+++ ch13/task-manager/src/task/TaskList.js
@@ -1,11 +1,46 @@
+import {useState, useEffect} from "react";
 import Task from "./Task";
 import TaskAdd from "./TaskAdd";
+import initialState from "./fixture";
+
+function getTasks() {
+  return JSON.parse(localStorage.getItem("tasks")) || initialState;
+}
 
 function TaskList() {
+  const [tasks, setTask] = useState(getTasks);
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+  }, [tasks]);
+  const addTask = title => {
+    setTask(tasks => tasks.concat(
+      {id: Math.random() * 1_000_000, title}
+    ));
+  };
+  const editTask = (id, title) => {
+    setTask(tasks => {
+      tasks.map(task => {
+        task.id === id ? {...task, title}: task
+      });
+    });
+  };
+  const deleteTask = id => {
+    setTask(tasks => {
+      tasks.filter(task => task.id !== id);
+    });
+  };
+
   return (
     <ol className="lane">
-      {tasks.map(task => <Task {...task}/>)} 
-      <TaskAdd/>
+      {tasks.map(task => (
+        <Task
+          key={task.id} 
+          title={task.title} 
+          editTask={editTask} 
+          deleteTask={deleteTask}
+        />
+      ))} 
+      <TaskAdd addTask={addTask}/>
     </ol>
   );
 }
