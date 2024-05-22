ee81c068d027083d184dd5281d68e473fcdac458
Ildar Shamiev <shamief@yandex.ru>
2024-05-22 16:36:27 +0300

    ch13 Task, fixture

--- ch13/task-manager/src/task/Task.js
+++ ch13/task-manager/src/task/Task.js
@@ -1,16 +1,39 @@
+import {useState} from "react";
 import Button from "../Button";
 import TaskHeader from "./TaskHeader";
+import TaskHeaderEdit from "./TaskHeaderEdit";
 
-function Task({title}) {
+function Task({title, taskID, editTask, deleteTask}) {
+  const [isEdit, setEdit] = useState(false);
+  const handleEdit = (id, value) => {
+    setEdit(false);
+    editTask(id, value);
+  };
   return (
     <li className="card">
-      <TaskHeader title={title}/>
+      {isEdit ? (
+        <TaskHeaderEdit 
+          title={title} 
+          taskID={taskID} 
+          handleEdit={handleEdit}
+        />
+      ) : (
+        <TaskHeader title={title}/>
+      )}
       <ul className="card-controls">
         <li>
-          <Button className="card-control" alt="Edit"/>
+          <Button
+            className="card-control" 
+            alt="Edit" 
+            onClick={() => setEdit(true)}
+          />
         </li>
         <li>
-          <Button className="card-control" alt="Delete">
+          <Button 
+            className="card-control" 
+            alt="Delete" 
+            onClick={() => deleteTask(taskID)}
+          />
         </li>
       </ul>
     </li> 
