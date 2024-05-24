3ea504be826ae0dccc9202a8dfee6b309140b282
Ildar Shamiev <shamief@yandex.ru>
2024-05-24 21:24:26 +0300

    ch13 TaskList

--- ch13/task-manager/src/task/TaskList.js
+++ ch13/task-manager/src/task/TaskList.js
@@ -1,43 +1,79 @@
-import {useState, useEffect} from "react";
+import {useReducer, useEffect} from "react";
 import Task from "./Task";
 import TaskAdd from "./TaskAdd";
+import TaskContext from "./TaskContext";
 import initialState from "./fixture";
 
 function getTasks() {
   return JSON.parse(localStorage.getItem("tasks")) || initialState;
 }
 
+function reducer(tasks, {type, ...rest}) {
+  switch (type) {
+    case "addTask":
+      return tasks.concat({id: Math.random() * 1_000_000, steps: [], ...rest});
+    case "editTask":
+      const {id, title} = rest;
+      return tasks.map(task => task.id === id ? {...task, title}: task);
+    case "deleteTask":
+      return tasks.filter(task => task.id !== rest.id);
+    case "addStep":
+      const addStep = (task, title) => {
+        task.steps = task.steps.concat(
+          {id: Math.random() * 1_000_000,
+            title, 
+            isDone: false
+          }
+        );
+        return task;
+      };
+      return tasks.map(task => {
+        return task.id === rest.taskID ?
+          addStep(task, rest.title) : task;
+      });
+    case "editStep":
+      const {stepID, taskID, ...props} = rest;
+      const updateStep = step => {
+        return step.id === stepID ?
+          {...step, ...props} : step
+      };
+      return tasks.map(task => {
+        return task.id === taskID ?
+          {...task,
+            steps: task.steps.map(step => updateStep(step))
+          } : task
+      });
+    case "deleteStep":
+      const deleteStep = (task, id) => {
+        task.steps = task.steps.filter(step => step.id !== id);
+        return task;
+      };
+      return tasks.map(task => {
+        return task.id === rest.taskID ? 
+          deleteStep(task, rest.stepID) : task
+      });
+    default:
+      return tasks;
+  }
+}
+
 function TaskList() {
-  const [tasks, setTasks] = useState(getTasks);
+  const [tasks, dispatch] = useReducer(reducer, getTasks());
   useEffect(() => {
     localStorage.setItem("tasks", JSON.stringify(tasks))
   }, [tasks]);
-  const addTask = title => {
-    setTasks(tasks => tasks.concat(
-      {id: Math.random() * 1_000_000, title}
-    ));
-  };
-  const editTask = (id, title) => {
-    setTasks(tasks => tasks.map(task => (
-      task.id === id ? {...task, title}: task
-    )));
-  };
-  const deleteTask = id => {
-    setTasks(tasks => tasks.filter(task => task.id !== id));
-  };
 
   return (
     <ol className="lane">
-      {tasks.map(task => (
-        <Task
-          key={task.id} 
-          taskID={task.id}
-          title={task.title} 
-          editTask={editTask} 
-          deleteTask={deleteTask}
-        />
-      ))} 
-      <TaskAdd addTask={addTask}/>
+      <TaskContext.Provider value={dispatch}>
+        {tasks.map(task => (
+          <Task
+            key={task.id}
+            task={task} 
+          />
+        ))} 
+        <TaskAdd/>
+      </TaskContext.Provider>
     </ol>
   );
 }
