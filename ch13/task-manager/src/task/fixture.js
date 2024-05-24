f7e1d9213f8856709296f49bc61e838e6f8235c6
Ildar Shamiev <shamief@yandex.ru>
2024-05-24 21:26:44 +0300

    ch13 edit fixture

--- ch13/task-manager/src/task/fixture.js
+++ ch13/task-manager/src/task/fixture.js
@@ -1,6 +1,14 @@
 const initialState = [
-  {id: 1, title: "Make task manager"},
-  {id: 2, title: "Now add some more tasks"},
+  {
+    id: 1,
+    title: "Make task manager",
+    steps: [{id: 10, title: "step1", isDone: true}],
+  },
+  {
+    id: 2,
+    title: "Now add some more tasks",
+    steps: [{id: 12, title: "step1", isDone: false}],
+  },
 ];
 
 export default initialState;
