e0ebf6939fe8e15d9998323a9115cf57048aa101
Ildar Shamiev <shamief@yandex.ru>
2024-05-29 12:56:04 +0300

    ch13 add task progress

--- ch13/task-manager/src/task/fixture.js
+++ ch13/task-manager/src/task/fixture.js
@@ -2,11 +2,13 @@ const initialState = [
   {
     id: 1,
     title: "Make task manager",
+    progress: "100",
     steps: [{id: 10, title: "step1", isDone: true}],
   },
   {
     id: 2,
     title: "Now add some more tasks",
+    progress: "0",
     steps: [{id: 12, title: "step1", isDone: false}],
   },
 ];
